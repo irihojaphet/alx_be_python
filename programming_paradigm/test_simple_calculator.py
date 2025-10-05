0ed0af86099ddf2793634b8fb9a020ea9b46b46f
irihojaphet <irihojaja@yahoo.com>
2025-10-05 23:19:53 +0200

    programming paradigm unity test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,20 +1,31 @@
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
+import unittest
+from simple_calculator import SimpleCalculator
 
-    def add(self, a, b):
-        """Return the addition of a and b."""
-        return a + b
 
-    def subtract(self, a, b):
-        """Return the subtraction of b from a."""
-        return a - b
+class TestSimpleCalculator(unittest.TestCase):
 
-    def multiply(self, a, b):
-        """Return the multiplication of a and b."""
-        return a * b
+    def setUp(self):
+        self.calc = SimpleCalculator()
 
-    def divide(self, a, b):
-        """Return the division of a by b. Returns None if b is zero."""
-        if b == 0:
-            return None
-        return a / b
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(5, 10), -5)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 5), -10)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5.0)
+        self.assertEqual(self.calc.divide(9, 3), 3.0)
+
+    def test_division_by_zero(self):
+        self.assertIsNone(self.calc.divide(10, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
